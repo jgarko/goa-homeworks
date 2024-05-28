0f24db635aa496d4b9321dde85913b0ad97d9066
jgarko <georgejgarkava2010@gmail.com>
2024-05-28 20:57:55 +0400

    commit

---
+++ Day 15/classwork/classwork.py
@@ -0,0 +1,8 @@
+#მომხმარებელს უნდა შემოატანინოთ რიცხვი და შემდეგ უნდა დაბეჭდოთ არის თუ არა ეს რიცხვი ხუთის ჯერადი
+
+num = int(input(" enter your num: "))
+
+if num % 5 == 0:
+    print(" this number is a multipule of five")
+else:
+    print(" this number is not a multiple of five")
